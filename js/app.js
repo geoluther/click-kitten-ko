f63eb9da93f964136cda51adc8c9f76a6b1294d6
George <george.figgs@gmail.com>
2015-03-10 11:24:00 -0600

    edits

--- js/app.js
+++ js/app.js
@@ -1,18 +1,64 @@
 // knockout js file
-var Cat = function() {
+var initialCats = [
+{
+	clickCount: 0,
+	name: "Tabby",
+	imgSrc: "https://placeimg.com/400/400/animals",
+	imageAttribution: "https:placeimg.com",
+	nickNames: ["King Tabby", "Tabberious Maximus", "Killer", "El Chapo"]
+},
 
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable("Tabby");
-	this.imgSrc = ko.observable("http://placeimg.com/300/300/animals");
-	this.imageAttribution = ko.observable("http://placeimg.com");
-	this.nickNames = ko.observableArray(["Killer", "Spot", "Lambchop", "Spunky"]);
+{
+	clickCount: 0,
+	name: "Tigger",
+	imgSrc: "https://placeimg.com/400/400/animals",
+	imageAttribution: "https:placeimg.com",
+	nickNames: ["Mr. Bounce", "The Nemesis", "Stripe", "Rubber Tush"]
+},
+
+
+{
+	clickCount: 0,
+	name: "sleepy",
+	imgSrc: "https://placeimg.com/400/400/animals",
+	imageAttribution: "https:placeimg.com",
+	nickNames: ["Mr. Bounce", "The Nemesis", "Stripe", "Rubber Tush"]
+},
+
+{
+	clickCount: 0,
+	name: "sneezy",
+	imgSrc: "https://placeimg.com/400/400/animals",
+	imageAttribution: "https:placeimg.com",
+	nickNames: ["Mr. Bounce", "The Nemesis", "Stripe", "Rubber Tush"]
+},
+
+{
+	clickCount: 0,
+	name: "stinky",
+	imgSrc: "https://placeimg.com/400/400/animals",
+	imageAttribution: "https:placeimg.com",
+	nickNames: ["Mr. Poo", "The Nemesis", "Stripe", "Rubber Tush"]
+},
+
+];
+
+
+var Cat = function(data) {
+
+	this.clickCount = data.clickCount;
+	this.name = data.name;
+	this.imgSrc = data.imgSrc;
+	this.imageAttribution = data.imageAttribution;
+	this.nickNames = data.nickNames;
 
 	this.level = ko.computed(function() {
 
-		var cc = this.clickCount();
+		var cc = this.clickCount;
+		
 		var title = "Novice";
 
-		if ( cc > 5 && cc <= 10 ){
+		if ( cc > 5 && cc <= 10 ) {
 			title = "Advanced Beginner";
 		} else	if ( cc > 10 && cc <= 25 ) {
 			title = "Semi-Pro";
@@ -30,10 +76,27 @@ var Cat = function() {
 
 var ViewModel = function() {
 
-	this.currentCat = ko.observable(new Cat);
+	var self = this;
+
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem) {
+		self.catList.push(new Cat(catItem) );
+	});
+	
+	this.currentCat = ko.observable(this.catList()[0]);
 
 	this.incrementCounter =  function() {
-		this.clickCount(this.clickCount() + 1 );
+		self.currentCat().clickCount += 1;
+		console.log(self.currentCat().clickCount);
+	}	
+
+	this.setCurrent = function(index) {
+		self.currentCat(self.catList()[index]);
+	}
+
+	this.clickLog = function() {
+		console.log("you clicked the list");
 	}
 
 };
