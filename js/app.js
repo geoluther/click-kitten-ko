eab2bbd586a82f0367865b3c68b98d7bab9bf597
George <my latest commit>
2015-03-10 21:45:19 -0600

    update working js

--- js/app.js
+++ js/app.js
@@ -1,41 +1,99 @@
 // knockout js file
-var Cat = function() {
+var initialCats = [
+	{
+		clickCount: 0,
+		name: "Tabby",
+		imgSrc: "https://placeimg.com/400/400/animals",
+		imageAttribution: "https:placeimg.com",
+		nickNames: ["King Tabby", "Tabberious Maximus", "Killer", "El Chapo"]
+	},
 
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable("Tabby");
-	this.imgSrc = ko.observable("http://placeimg.com/300/300/animals");
-	this.imageAttribution = ko.observable("http://placeimg.com");
-	this.nickNames = ko.observableArray(["Killer", "Spot", "Lambchop", "Spunky"]);
+	{
+		clickCount: 0,
+		name: "Tigger",
+		imgSrc: "https://placeimg.com/400/400/animals",
+		imageAttribution: "https:placeimg.com",
+		nickNames: ["Mr. Bounce", "The Nemesis", "Stripe", "Rubber Tush"]
+	},
+
+	{
+		clickCount: 0,
+		name: "Sleepy",
+		imgSrc: "https://placeimg.com/400/400/animals",
+		imageAttribution: "https:placeimg.com",
+		nickNames: ["Mr. Bounce", "The Nemesis", "Stripe", "Rubber Tush"]
+	},
+
+	{
+		clickCount: 0,
+		name: "Sneezy",
+		imgSrc: "https://placeimg.com/400/400/animals",
+		imageAttribution: "https:placeimg.com",
+		nickNames: ["Mr. Bounce", "The Nemesis", "Stripe", "Rubber Tush"]
+	},
+
+	{
+		clickCount: 0,
+		name: "Stinky",
+		imgSrc: "https://placeimg.com/400/400/animals",
+		imageAttribution: "https:placeimg.com",
+		nickNames: ["Mr. Poo", "The Nemesis", "Stripe", "Rubber Tush"]
+	}
+];
+
+var Cat = function(data) {
+
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = data.name;
+	this.imgSrc = data.imgSrc;
+	this.imageAttribution = data.imageAttribution;
+	this.nickNames = data.nickNames;
 
 	this.level = ko.computed(function() {
 
 		var cc = this.clickCount();
-		var title = "Novice";
+		var title = "Tail Wagger";
 
-		if ( cc > 5 && cc <= 10 ){
-			title = "Advanced Beginner";
+		if ( cc > 5 && cc <= 10 ) {
+			title = "Ear Scratcher";
 		} else	if ( cc > 10 && cc <= 25 ) {
-			title = "Semi-Pro";
+			title = "Neck Hugger";
 		} else if ( cc > 25 && cc <= 50 ) {
-			title = "Pro";
+			title = "Leg Twitcher";
 		} else  if ( cc > 50 ){
-			title = "Super Cuddy";
+			title = "Super Cuddler";
 		}
-
 		return title;
-
 	}, this);
 
 };
 
 var ViewModel = function() {
 
-	this.currentCat = ko.observable(new Cat);
+	var self = this;
+
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem) {
+		self.catList.push(new Cat(catItem) );
+	});
+	
+	this.currentCat = ko.observable(this.catList()[0]);
 
 	this.incrementCounter =  function() {
-		this.clickCount(this.clickCount() + 1 );
+		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+		console.log(this.clickCount);
+	}	
+
+	this.setCurrent = function(clickedCat) {
+		self.currentCat(clickedCat);
+		console.log(cat.name);
 	}
 
+	this.clickLog = function(cat) {
+		console.log(cat.name);
+		console.log("you clicked the list");
+	}
 };
 
 ko.applyBindings(new ViewModel());
